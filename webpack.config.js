360c9a91c76c32449c66411158f2991af46465c9
Anton Kravchenko <antosha.kravchenko@gmail.com>
2020-07-17 01:27:38 +0300

    v0.0.6 - fixed module build

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 module.exports = {
   entry: './main.ts',
   mode: 'production',
+  target: 'node',
   module: {
     rules: [{ test: /\.ts?$/, use: 'ts-loader', exclude: /node_modules/ }],
   },
@@ -11,5 +12,6 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2',
   },
 };
